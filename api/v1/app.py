9b71c1f289b15b300512f0f05ea922a62f841353
PhYdrogen <Fossecruor@gmail.com>
2023-04-12 18:32:15 +0200

    Critical change, remove debug from app run in app

--- api/v1/app.py
+++ api/v1/app.py
@@ -25,4 +25,4 @@ def _handle_api_error(ex):
 if __name__ == "__main__":
     host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
     port = int(os.environ.get("HBNB_API_PORT", 5050))
-    app.run(debug=True, host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True)
