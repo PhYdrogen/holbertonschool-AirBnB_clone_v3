9740fa2bd65c786200f291a5f9e8cbac8f1f6c44
Valentin <5619@holbertonstudents.com>
2023-04-12 01:41:18 -0700

    update file app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, Blueprint, render_template, abort
 from models import storage
-from api.v1
+from api.v1.views import app_views
+
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext("/")
+def app_teardown():
+    storage.close()
+
+if __name__ == "__main__":
+    host = "0.0.0.0" #HBNB_API_HOST
+    port = 5000 #HBNB_API_PORT
+    threaded=True
