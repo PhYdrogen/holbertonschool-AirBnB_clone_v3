9cc5c37e783f784521fe730c9b569d78c9f9867d
Gabriel <fossecruor@gmail.com>
2023-04-12 05:53:13 -0700

    Retrives all State in storage

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,11 @@ from models import storage
 from flask import jsonify
 from models import storage
 from api.v1.views import app_views
+from models.state import State
 
 @app_views.route('/states', strict_slashes=False)
 def state_page():
-    return jsonify('hello')
+    arr = []
+    for values in storage.all(State).values():
+      arr.append(values.to_dict())
+    return jsonify(arr)
