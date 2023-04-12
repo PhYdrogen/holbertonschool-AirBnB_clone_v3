1bf605fd22cb71baefa08b40c8faeeb1e3e06cd4
Gabriel <fossecruor@gmail.com>
2023-04-12 04:19:14 -0700

    Reformated the code for pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+""" Index where route is defined """
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', strict_slashes=False)
 def index():
-  status = {'status': 'OK'}
-  return jsonify(status)
+    status = {'status': 'OK'}
+    return jsonify(status)
