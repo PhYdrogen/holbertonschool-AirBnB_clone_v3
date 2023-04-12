cba0a9ee8dcddb0cef0bc04129601a647ab68f00
Gabriel <fossecruor@gmail.com>
2023-04-12 01:40:06 -0700

    imported app views, create route for it

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,4 @@
 from flask import Blueprint
 
-app_views = Blueprint('admin', __name__, static_folder='static')
+app_views = Blueprint('app_views', __name__,
+                        url_prefix='/api/v1')
