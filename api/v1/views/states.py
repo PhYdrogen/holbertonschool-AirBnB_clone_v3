c699004d106bd5a84ae856567913f7b0105f3554
Gabriel <fossecruor@gmail.com>
2023-04-12 13:17:12 -0700

    Added PUT and POST function

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -6,8 +6,8 @@ from models import storage
 from api.v1.views import app_views
 from models.state import State
 
-@app_views.route('/states', strict_slashes=False)
-@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET', 'DELETE', 'PUT', 'POST'])
+@app_views.route('/states', strict_slashes=False, methods=['GET','POST'])
+@app_views.route('/states/<state_id>', strict_slashes=False, methods=['GET', 'DELETE', 'PUT'])
 def state_page(state_id=None):
     print(request.method)
     if request.method == 'GET':
@@ -21,12 +21,27 @@ def state_page(state_id=None):
                 return jsonify(storage.get(State, state_id).to_dict())
             except AttributeError:
                 abort(404)
+
     elif request.method == 'DELETE':
         obj = storage.get(State, state_id)
         storage.delete(obj)
         storage.save()
         return jsonify({}), 200
+
     elif request.method == 'PUT':
-        pass
+        obj = storage.get(State, state_id)
+        for k, v in request.get_json().items():
+            setattr(obj, k, v)
+        storage.save()
+        return jsonify(obj.to_dict()), 200
+
     elif request.method == 'POST':
-        pass
+        req_dict = request.get_json().items()
+        if not req_dict.has_key('name'):
+            return 'Missing name', 400
+        for req_key, req_value in request.get_json().items():
+            for state_obj in storage.all(State).values():
+                state_dict = state_obj.to_dict()
+                if req_value == state_dict.get(req_key):
+                    return jsonify(state_dict), 201
+        return '{}', 200
