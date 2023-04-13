8227ee4d208255dfcf97555a12ff17f28aa91370
Gabriel <fossecruor@gmail.com>
2023-04-13 07:09:51 -0700

    Added view User and init

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -8,4 +8,44 @@ from models.user import User
 
 method_lt = ['GET', 'POST', 'PUT', 'DELETE']
 
+@app_views.route('/users', strict_slashes=False,  methods=method_lt)
+@app_views.route('/users/<user_id>', strict_slashes=False,  methods=method_lt)
+def user_page(user_id=None):
+    if request.method == "GET":
+        if user_id is None:
+            ret = [obj.to_dict() for obj in storage.all(User).values()]
+            return jsonify(ret), 200
+    elif request.method == "DELETE":
+        UserObj = storage.get(User, user_id)
+        if UserObj is None:
+            abort(404)
+        UserObj.delete()
+        storage.save()
+        return {}, 200
+    elif request.method == "POST":
+        if not request.is_json:
+            return 'Not a JSON', 400
+        req_dict = request.get_json()
 
+        if 'email' not in req_dict:
+            return 'Missing email', 400
+        if 'password' not in req_dict:
+            return 'Missing password', 400
+
+        for UserObj in storage.all(User).values() :
+            UsD = UserObj.to_dict()
+            if req_dict.get('email') == UsD.get('email') and req_dict.get('password') == UsD.get('password'):
+                return jsonify(UsD), 201
+        abort(404)
+    elif request.method == "PUT":
+        if user_id is None:
+            abort(404)
+        obj = storage.get(User, user_id)
+        if obj is None:
+            abort(404)
+        if not request.is_json:
+            return 'Not a JSON', 400
+
+        for k, v in request.get_json():
+            setattr(obj, k, v)
+        return jsonify(obj.to_dict()), 200
